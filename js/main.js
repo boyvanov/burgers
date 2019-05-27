60bc705ffa11f094d0d04e5090a5cfc763b5836a
Sergey Boyvanov <free20@yandex.ru>
2019-05-27 14:51:32 +0300

    added animation

--- js/main.js
+++ js/main.js
@@ -5,10 +5,35 @@ let menu = (function (options) {
 
   let itemList = document.getElementById('nav__list').children;
 
+  let counter = 0,
+    flag = false;
+
+  let startMenuAnimation = function startMenuAnimation() {
+    let elem = itemList[counter];
+
+    elem.classList.toggle('slideInUp');
+    counter++;
+    if (counter < itemList.length) {
+      setTimeout(startMenuAnimation, 100);
+    }
+    if (counter === itemList.length) {
+      counter = 0;
+    }
+  }
+
   let _toggleMenu = function (e) {
     button.classList.toggle('hamburger-menu-icon--active');
     menu.classList.toggle('hamburger-menu--active');
     body.classList.toggle('locked');
+
+    startMenuAnimation();
+
+    if (flag) {
+      flag = false;
+    } else {
+      flag = true;
+    }
+    // flag ? flag = false : flag = true;
   }
 
   let addListeners = function () {
@@ -16,11 +41,18 @@ let menu = (function (options) {
 
     menu.addEventListener('click', function (e) {
       target = e.target;
-      if (target.classList == 'nav__link') {
+      if (target.classList.contains('nav__link')) {
         _toggleMenu();
       }
     })
-  }
+  };
+
+  document.addEventListener('keydown', function (e) {
+    if ((e.keyCode == 27) && flag) {
+      _toggleMenu();
+    }
+
+  });
 
   return {
     openMenu: addListeners
